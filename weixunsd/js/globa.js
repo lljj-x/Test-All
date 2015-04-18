0396490f1e0ad089c53ca679b22b3ece86b6fca7
Liu.Jun <870395192@qq.com>
2015-04-18 16:34:03 +0800

    xx

--- weixunsd/js/globa.js
+++ weixunsd/js/globa.js
@@ -50,4 +50,202 @@ $(document).ready(function(){
             slideMenu();
         }
     });
-})
+});
+
+var getImgNum = function(){
+    var currentWidth = $().getBgWidth();
+    var imgNum ;
+    if(currentWidth >= 1500){
+        imgNum = 6;
+    }else if(currentWidth < 1499 && currentWidth >= 1200){
+        imgNum = 5;
+    }else if(currentWidth < 1199 && currentWidth >=992 ){
+        imgNum = 5;
+    }else if(currentWidth < 991 && currentWidth >=768 ){
+        imgNum = 4;
+    }else if(currentWidth < 767 && currentWidth >=480 ){
+        imgNum = 2;
+    }else{
+        imgNum = 1;
+    }
+    return imgNum;
+};
+
+/**
+(function (window, document, exportName, $) {
+    var defaults = {
+        isAutoPlay: false,				    // 是否自动轮播
+        isLoop: false,                      // 循环
+        idDisControllBtn: false,            // 显示控制按钮
+        interval: '3000',				    // 如果自动轮播 设置间隔时间 (ms)
+        imgPageNums: 1,				        // 每次滚动多少张图片
+        prevBtnId: 'left_button',		    // 上一页按钮 Id 名称
+        nextBtnId: 'right_button',		    // 下一页按钮 Id 名称
+        switchDivId: 'index-top-pro-list', 	// 指示器放在那个div中
+        speed: 500,                         // 动画时间
+        isTouch: true,                     // 触屏
+        easing: 'easeOutSine'               //需要animate easing 扩展，或者jquery ui
+    }
+
+    function Carousel(jQueryElement, options) {
+        options = options || {};
+        this.options = $.extend({}, defaults, options);
+        this.jQelement = jQueryElement;
+        this.contentList = $(".l_content_list", this.jQelement);
+        this.imgNums = $(".l_content_list li", this.jQelement).length;
+
+        this.viewSize = 0;
+        this.imgPages = 0;
+        this.imgCurrentPage = 1;
+//        jQueryElement.css({
+//            "overflow" : "hidden"
+//        });
+        this.init();
+        return this;
+    }
+
+    Carousel.prototype = {
+        checkDisable: function () {
+            if (this.imgCurrentPage == 1) {
+                $("#" + this.options.prevBtnId).addClass("disable");
+            } else {
+                if ($("#" + this.options.prevBtnId).hasClass("disable")) {
+                    $("#" + this.options.prevBtnId).removeClass("disable");
+                }
+            }
+            if (this.imgCurrentPage == this.imgPages) {
+                $("#" + this.options.nextBtnId).addClass("disable");
+            } else {
+                if ($("#" + this.options.nextBtnId).hasClass("disable")) {
+                    $("#" + this.options.nextBtnId).removeClass("disable");
+                }
+            }
+        },
+
+        init: function () {
+            var myThis = this;
+            myThis.changeParams();
+            var gotoSection = function (thisIndex) {
+                if (thisIndex >= myThis.imgPages - 1) {
+                    thisIndex = myThis.imgPages - 1;
+                }
+                if (thisIndex <= 0) {
+                    thisIndex = 0;
+                }
+
+                myThis.contentList.stop().animate({"left": -(myThis.viewSize * (thisIndex))}, myThis.options.speed, myThis.options.easing);
+                myThis.imgCurrentPage = thisIndex + 1;
+                if (myThis.options.idDisControllBtn) {
+                    $("#" + myThis.options.switchDivId).children("span").eq(thisIndex).addClass("current").siblings().removeClass("current");
+                }
+                myThis.checkDisable();
+            }
+
+            // 触屏手势
+            if (myThis.options.isTouch) {
+                var hammerCon = new Hammer(myThis.jQelement[0]);
+                hammerCon.get("pan").set({direction: Hammer.DIRECTION_HORIZONTAL});
+                var _left = 0;
+                hammerCon.on("panstart", function () {
+                    _left = parseInt(myThis.contentList.css("left").replace("px", ""));
+                });
+                hammerCon.on("panend pancancel", function (ev) {
+                    if (Math.abs(ev.deltaX) >= ((myThis.viewSize / 2 >= 50) ? 50 : (myThis.viewSize / 2))) {
+                        if (ev.deltaX > 0) {
+                            gotoSection(myThis.imgCurrentPage - 2);
+                        } else {
+                            gotoSection(myThis.imgCurrentPage);
+                        }
+                    } else {
+                        myThis.contentList.stop().animate({"left": _left + "px"}, myThis.options.speed, myThis.options.easing);
+                    }
+                });
+                hammerCon.on("panmove", function (ev) {
+                    myThis.contentList.css({
+                        left: (_left + ev.deltaX) + "px"
+                    });
+                });
+            }
+
+            if (myThis.options.idDisControllBtn) {
+                // 添加指示器
+                var htmlInnerSwitch = '<span class="current">1</span>';
+                for (var i = 1; i <= myThis.imgPages; i++) {
+                    if (i > 1) {
+                        htmlInnerSwitch = htmlInnerSwitch + '<span>' + i + '</span>';
+                    }
+                };
+                $("#" + myThis.options.switchDivId).append($(htmlInnerSwitch));
+                // 指示器点击事件
+                $("#" + myThis.options.switchDivId).children("span").on("click", function () {
+                    var thisIndex = $(this).index();
+                    gotoSection(thisIndex);
+                });
+            }
+
+            $("#" + myThis.options.prevBtnId).on("click", function (event) {
+                event.preventDefault();
+                // 上一页事件
+                if (!myThis.contentList.is(":animated")) {
+                    if (myThis.imgCurrentPage == 1) {
+                        if (myThis.options.isLoop) {
+                            myThis.contentList.animate({"left": "-=" + (myThis.viewSize * (myThis.imgPages - 1))}, myThis.options.speed, myThis.options.easing)
+                            myThis.imgCurrentPage = myThis.imgPages;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        myThis.contentList.animate({"left": "+=" + myThis.viewSize}, myThis.options.speed, myThis.options.easing)
+                        myThis.imgCurrentPage--;
+                    }
+                    $("#" + myThis.options.switchDivId).children("span").eq(myThis.imgCurrentPage - 1).addClass("current").siblings().removeClass("current");
+                }
+                myThis.checkDisable();
+            });
+            $("#" + myThis.options.nextBtnId).on("click", function (event) {
+                event.preventDefault();
+                // 下一页事件
+                if (!myThis.contentList.is(":animated")) {
+                    if (myThis.imgCurrentPage == myThis.imgPages) {
+                        if (myThis.options.isLoop) {
+                            this.contentList.animate({"left": "+=" + myThis.viewSize * (myThis.imgPages - 1)}, myThis.options.speed, myThis.options.easing)
+                            myThis.imgCurrentPage = 1;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        myThis.contentList.animate({"left": "-=" + myThis.viewSize}, myThis.options.speed, myThis.options.easing)
+                        myThis.imgCurrentPage++;
+                    }
+                    $("#" + myThis.options.switchDivId).children("span").eq(myThis.imgCurrentPage - 1).addClass("current").siblings().removeClass("current");
+                }
+                myThis.checkDisable();
+            });
+
+            // 自动轮播
+            if (myThis.options.isAutoPlay) {
+                function autoPlay() {
+                    $("#" + myThis.options.nextBtnId).trigger("click");
+                };
+                var thisId = setInterval(autoPlay, myThis.options.interval);
+                this.jQelement.attr({"intervalid": thisId});
+            }
+        },
+        changeParams: function (changeParams) {
+            changeParams = changeParams || {};
+            this.options = $.extend({}, this.options, changeParams);
+            this.viewSize = $(".l_content_list li", this.jQelement).outerWidth(true) * this.options.imgPageNums;
+            this.imgPages = Math.ceil(this.imgNums / this.options.imgPageNums);
+            this.imgCurrentPage = 1;
+            this.contentList.css({
+                position: "absolute",
+                top: "0px",
+                left: "0px",
+                width: this.viewSize * this.imgPages + "px"
+            });
+            this.checkDisable();
+        }
+    };
+    window[exportName] = Carousel;
+}(window, document, 'Carousel', jQuery));
+
--- weixunsd/js/index.js
+++ weixunsd/js/index.js
@@ -2,7 +2,8 @@
  * Created by LiuJun on 15-4-5下午2:06.
  */
 weixunData = {
-    'weixun-anli-jingji' : '<div class="slide fp-slide fp-table" data-anchor="slide-anli1" style=""><div class="fp-tableCell" style="height:100%"><div class="table-bg-relative"><div class="table-bg-absolute"><div class="weixun-content-bg"><div class="weixun-content"><div class="section-title">背景</div><div class="section-content">对京基集团旗下昂驰动画的作品进行推广，为后期京基昂驰大电影的宣传做铺垫，最终建立京基昂驰企业品牌与动画IP形象。</div></div></div></div><div class="weixun-icon p-bottom"><a class="a-left weixun-prev" href="javascript:void(0);"><span class="icon-arrow-left"></span></a> <a class="a-right weixun-next" href="javascript:void(0);"><span class="icon-arrow-right"></span></a></div></div></div></div><div class="slide fp-slide fp-table" data-anchor="slide-anli2" style=""><div class="fp-tableCell" style="height:100%"><div class="table-bg-relative"><div class="table-bg-absolute"><div class="weixun-content-bg"><div class="weixun-content"><div class="section-title">挑战</div><div class="section-content">市场动画片种类繁多，国产动画口碑不佳，昂驰豆豆动画的知名度不高，推广难度较大。</div></div></div></div><div class="weixun-icon p-bottom"><a class="a-left weixun-prev" href="javascript:void(0);"><span class="icon-arrow-left"></span></a> <a class="a-center weixun-first" href="javascript:void(0);"><span class="icon-indent-left"></span></a> <a class="a-right weixun-next" href="javascript:void(0);"><span class="icon-arrow-right"></span></a></div></div></div></div><div class="slide fp-slide fp-table" data-anchor="slide-anli3" style=""><div class="fp-tableCell" style="height:100%"><div class="table-bg-relative"><div class="table-bg-absolute"><div class="weixun-content-bg"><div class="weixun-content"><div class="section-title">创意</div><div class="section-content">1、与微博大V合作，利用双方的粉丝进行流量共享与互导，集聚更多粉丝，成功塑造昂驰豆豆的“二货”精神，推出#二货快乐基金#<br>2、在目前最火爆的微信中，利用最新技术手段开发 #二货指数# HtmL5游戏，增加粉丝互动性，达到微博、微信相互导流量。<br>结果：微博粉丝20万， 全网曝光量1.3亿<br>微信粉丝增加3万，平均阅读量4000。<br>成功加强了豆豆的2B形象的塑造，拉近了品牌与粉丝的距离，建立豆豆社会化品牌新形象。</div></div></div></div><div class="weixun-icon p-bottom"><a class="a-left weixun-prev" href="javascript:void(0);"><span class="icon-arrow-left"></span></a> <a class="a-right weixun-first" href="javascript:void(0);"><span class="icon-indent-left"></span></a></div></div></div></div>',
+    'weixun-anli-jingji' : '<div class="slide fp-slide fp-table" data-anchor="slide-anli1" style="background-image:url(images/section_image/bg_6.jpg)"> <div class="bg-color" style="background-color:#0089d0"></div> <div class="fp-tableCell" style="height:100%"> <div class="table-bg-relative"> <div class="table-bg-absolute column-2 clearfix"> <div class="left left-image"> <div class="anli-left-content"> <h3 class="left-title section-title">京基昂驰豆豆</h3> <div class="section-content left-title-2"> 推广方案 </div> </div> </div> <div class="weixun-content-bg left arrowed" style="background-color:#008ad0"> <div class="weixun-content"> <div class="section-title"> 背景 </div> <div class="section-content"> 对京基集团旗下昂驰动画的作品进行推广，为后期京基昂驰大电影的宣传做铺垫，最终建立京基昂驰企业品牌与动画IP形象。 </div> <div class="section-title"> 挑战 </div> <div class="section-content"> 市场动画片种类繁多，国产动画口碑不佳，昂驰豆豆动画的知名度不高，推广难度较大。 </div> <div class="section-title"> 创意 </div> <div class="section-content"> 1、与微博大V合作，利用双方的粉丝进行流量共享与互导，集聚更多粉丝，成功塑造昂驰豆豆的“二货”精神，推出#二货快乐基金#<br> 2、在目前最火爆的微信中，利用最新技术手段开发 #二货指数# HtmL5游戏，增加粉丝互动性，达到微博、微信相互导流量。<br> 结果：微博粉丝20万， 全网曝光量1.3亿<br> 微信粉丝增加3万，平均阅读量4000。<br> 成功加强了豆豆的2B形象的塑造，拉近了品牌与粉丝的距离，建立豆豆社会化品牌新形象。 </div> </div> </div> </div> <div class="anli-bottom-controls"> <div class="weixun-icon p-bottom"> <a class="a-right weixun-next" href="javascript:void(0);"><span class="icon-arrow-right"></span></a> </div> </div> </div> </div> </div> <div class="slide fp-slide fp-table" data-anchor="slide-anli3" style="background-image:url(images/section_image/bg_6.jpg)"> <div class="bg-color" style="background-color:#0089d0"></div> <div class="fp-tableCell" style="height:100%"> <div class="weixun-anli-bg"> <div class="weixun-anli-img-bg"> <div id="anli-img-list-bg"> <div class="anli-img_list l_content_list"> <ul class="clearfix"> <li> <img src="images/anli/dd_1.jpg" alt="京基昂驰豆豆" /> </li> <li> <img src="images/anli/dd_2.jpg" alt="京基昂驰豆豆" /> </li> <li> <img src="images/anli/dd_3.jpg" alt="京基昂驰豆豆" /> </li> <li> <img src="images/anli/dd_4.jpg" alt="京基昂驰豆豆" /> </li> <li> <img src="images/anli/dd_5.jpg" alt="京基昂驰豆豆" /> </li> </ul> </div> </div> <div class="con_button disable" id="left_button"><span class="icon-arrow-left"></div> <div class="con_button" id="right_button"><span class="icon-arrow-right"></div> </div> </div> <div class="anli-bottom-controls"> <div class="weixun-icon p-bottom"> <a class="a-left weixun-prev" href="javascript:void(0);"><span class="icon-arrow-left"></span></a> </div> </div> </div> </div>',
+    'weixun-anli-o2o' : '<div class="slide fp-slide fp-table" data-anchor="slide-anli1" style="background-image:url(images/section_image/bg_7.jpg)"><div class="bg-color" style="background-color:#0089d0"></div><div class="fp-tableCell" style="height:100%"><div class="table-bg-relative"><div class="table-bg-absolute column-2 clearfix"><div class="left left-image"><div class="anli-left-content"><h3 class="left-title section-title">O2O之自我营销</h3><div class="section-content left-title-2">推广方案</div></div></div><div class="weixun-content-bg left arrowed" style="background-color:#008ad0"><div class="weixun-content"><div class="section-title">背景</div><div class="section-content">“双十一”到来，微讯时代用极具创意与吸引力的活动，结合线上与线下（O2O）的方法，展示公司品牌形象、粉丝互动，从而进行一次自我营销活动。</div><div class="section-title">挑战</div><div class="section-content">传统的双十一活动已让大众审美疲劳，如何通过大数据精准定位单身人士进行线上营销，线下集聚人气，活动的创意策划跟执行是关键。</div><div class="section-title">创意</div><div class="section-content"><strong>线上推广</strong>： “#颠覆十一，缘遇爱情#你是否愿意期待一场美丽的意外，感受一次怦然心动，一张影票，更是一次珍贵的缘分，单号的你是否会遇见双号的TA,还在等什么？转发评论@ 好友即有机会获得《单身男女2》首映日电影票，80后百恩伉俪将和你相遇深圳太平洋电影城（天利名城店）11号厅，见证你和TA的奇迹！（10多位大V主动转发）<br><br><strong>线下引爆</strong>：CEO现场自爆爱情史为噱头引入活动主旋律。<br>“单号男双号女”的座位方式，在纸条上写出对任意座位号上的人想说的话的游戏，增加现场的趣味性，调动观众的气氛。<br><br><strong>结果</strong>：通过微博进行人气聚集，参与报名人数1091人，最终筛选出50位单身男女，并且现场活动获得粉丝一致好评，最终将现场的粉丝与活动内容导入到微信上，使粉丝由微博到微信进行导流互动，成功塑造公司形象。</div></div></div></div><div class="anli-bottom-controls"><div class="weixun-icon p-bottom"><a class="a-right weixun-next" href="javascript:void(0);"><span class="icon-arrow-right"></span></a></div></div></div></div></div><div class="slide fp-slide fp-table" data-anchor="slide-anli3" style=""><div class="fp-tableCell" style="height:100%"><div class="table-bg-relative"><div class="table-bg-absolute"><div class="weixun-content-bg"><div class="weixun-content"><div class="section-title">创意</div><div class="section-content">1、与微博大V合作，利用双方的粉丝进行流量共享与互导，集聚更多粉丝，成功塑造昂驰豆豆的“二货”精神，推出#二货快乐基金#<br>2、在目前最火爆的微信中，利用最新技术手段开发 #二货指数# HtmL5游戏，增加粉丝互动性，达到微博、微信相互导流量。<br>结果：微博粉丝20万， 全网曝光量1.3亿<br>微信粉丝增加3万，平均阅读量4000。<br>成功加强了豆豆的2B形象的塑造，拉近了品牌与粉丝的距离，建立豆豆社会化品牌新形象。</div></div></div></div><div class="weixun-icon p-bottom"><a class="a-left weixun-prev" href="javascript:void(0);"><span class="icon-arrow-left"></span></a> <a class="a-right weixun-first" href="javascript:void(0);"><span class="icon-indent-left"></span></a></div></div></div></div>'
 };
 weixunResizeTimer = null;
 
@@ -27,11 +28,8 @@ var getResizePaddingTop = function(){
 }
 
 $.fn.resizeOverFlow = function(){
-//    var $_wbg = $(".weixun-content-bg");
-//    var parentHeight = $(window).height();
-    $(".weixun-content-bg").css({
+    $(".section").css({
         "paddingTop" : getResizePaddingTop() + "px"
-        // "height" : (parentHeight - getResizePaddingTop()) + "px"
     })
 }
 $(window).resize(function(){
@@ -42,23 +40,43 @@ $(window).resize(function(){
 });
 
 $.fn.addWeixunEvent = function(){
-    $(".section").each(function(){
-       var $section = $(this);
-//        $section.find(".weixun-prev").click(function(){
-//            $.fn.fullpage.moveSlideLeft();
-//        })
-//        $section.find(".weixun-next").click(function(){
-//            $.fn.fullpage.moveSlideRight();
-//        })
-//        $section.find(".weixun-first").click(function(){
-//            $.fn.fullpage.moveTo($section.index() + 1, 0);
-//        })
-//        $section.find(".weixun-down").click(function(){
-//            $.fn.fullpage.moveSectionDown();
-//        })
+    $(document).on("click",".ajax-content>a",function(e){
+        e.preventDefault();
+        var thisDataId = $(this).attr("getData");
+        $.fn.fullpage.weixunAddSlide('#' + $(this).closest(".slide").attr("id"),weixunData[thisDataId],$(this).closest(".section").index() + 1);
     });
-};
 
+    $(document).on("click",".weixun-prev",function(e){
+        $.fn.fullpage.moveSlideLeft();
+    })
+
+    $(document).on("click",".weixun-next",function(e){
+        $.fn.fullpage.moveSlideRight();
+    })
+    $(document).on("click",".weixun-first",function(e){
+        $.fn.fullpage.moveTo($(this).closest(".section").index() + 1, 0);
+    })
+    $(document).on("click",".weixun-down",function(e){
+        $.fn.fullpage.moveSectionDown();
+    });
+
+
+//
+//    var myCarousel = new Carousel($("#index-top-pro"));
+//    $("#index-top-pro").css("width","100%");
+//    imgNum = getImgNum();
+//    myCarousel.changeParams({
+//        imgPageNums: imgNum
+//    });
+//    $(window).on("resize",function(){
+//        myCarousel.changeParams({
+//            imgPageNums: getImgNum()
+//        });
+//    });
+
+
+
+};
 
 $(function () {
     var scrollingSpeed = 700;
@@ -89,8 +107,7 @@ $(function () {
         recordHistory: true,
         controlArrows: true,
         verticalCentered: true,
-        // paddingTop: getResizePaddingTop(),
-        paddingTop: 0,
+        paddingTop: getResizePaddingTop(),
         paddingBottom: '0',
         responsive: 0,
 
@@ -105,7 +122,6 @@ $(function () {
                     "background-color" : arrBgColor[0]
                 });
             }
-            $.fn.resizeOverFlow();
             $.fn.addWeixunEvent();
         },
         afterLoad: function(anchorLink, index){
@@ -131,25 +147,4 @@ $(function () {
             },scrollingSpeed);
         }
     });
-
-    $(document).on("click",".ajax-content>a",function(e){
-        e.preventDefault();
-        var thisDataId = $(this).attr("getData");
-        $.fn.fullpage.weixunAddSlide('#' + $(this).closest(".slide").attr("id"),weixunData[thisDataId],$(this).closest(".section").index() + 1);
-    });
-
-    $(document).on("click",".weixun-prev",function(e){
-        $.fn.fullpage.moveSlideLeft();
-    })
-
-    $(document).on("click",".weixun-next",function(e){
-        $.fn.fullpage.moveSlideRight();
-    })
-    $(document).on("click",".weixun-first",function(e){
-        $.fn.fullpage.moveTo($(this).closest(".section").index() + 1, 0);
-    })
-    $(document).on("click",".weixun-down",function(e){
-        $.fn.fullpage.moveSectionDown();
-    });
-
 })
