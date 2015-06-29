70c3b83d30b6cfa8229cbf45a0339af8c4cda6be
liujunchina <asdf123456>
2015-06-29 18:23:53 +0800

    other

--- test_all/text.php
+++ test_all/text.php
@@ -1,19 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: liu.jun
- * Date: 14-9-19
- * Time: 上午10:42
- */
 
-//echo phpinfo();
-//
-//echo phpinfo();
-//echo phpinfo();
-//echo "xxx";
-//
-//echo phpinfo();
-//echo phpinfo();
+$arrStr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+$outputFilename = "C:/Users/Administrator/Desktop/text.txt";
+$outputFp = fopen($outputFilename, 'w') or die("can't create file".$outputFilename);
 
-echo (int)"19995";
+ for($a = 0;$a<=25;$a++){
+    $one = $arrStr[$a];
+     for($b=0;$b<=25;$b++){
+         $two = $arrStr[$b];
+         for($c=0;$c<=25;$c++){
+             $three = $arrStr[$c];
+             for($d=0;$d<=25;$d++){
+                 $four = $arrStr[$d];
+                 echo $one . $two .$three . $four . '<br>';
+             }
+         }
+     }
+ }
