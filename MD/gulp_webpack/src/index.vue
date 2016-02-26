414eaa60e5ea813ccb93e73980ee3fd2356654fa
LiuJun <870395192@qq.com>
2016-02-26 19:08:07 +0800

    webpack + gulp config

---
+++ MD/gulp_webpack/src/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <header-component></header-component>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    import HeaderComponent from './components/HeaderComponent.vue'
+    export default{
+        data(){
+            return {
+                msg: 'hello vue'
+            }
+        }, components:{
+            HeaderComponent
+        }
+    };
+</script>
---
+++ MD/gulp_webpack/src/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Liu.Jun on 2016/2/26.
+ */
+(function () {
+    'use strict';
+    console.log("This ok");
+})();
