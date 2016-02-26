6e6f23415a9699d4cf54e15ab9b34e69ab031495
LiuJun <870395192@qq.com>
2016-02-26 19:08:07 +0800

    webpack + gulp config

---
+++ MD/gulp_webpack/src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>这里是头部信息{{msg}}</h1>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        }
+    }
+</script>
