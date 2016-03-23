f51ce4573ea4893a9e02f63990569ca42735de24
LiuJun <870395192@qq.com>
2016-03-23 16:43:39 +0800

    webpack

--- MD/avalon_webpack/webpack.config.js
+++ MD/avalon_webpack/webpack.config.js
@@ -1,24 +1,24 @@
 var webpack = require('webpack');
-var cssLoader = { test: /\.css$/, loader: "style-loader!css-loader" };
+var cssLoader = {test: /\.css$/, loader: "style-loader!css-loader"};
 
 module.exports = {
-    entry: './dev/index.js',
-    output: {
-        path: './static',
+    entry  : './dev/index.js',
+    output : {
+        path      : './static',
         publicPath: 'static/',
-        filename: 'build.js'
+        filename  : 'build.js'
     },
-    module: {
-        loaders: [
-          cssLoader,
-          { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
-          //{
-          //    test: /\.js$/,
-          //    // excluding some local linked packages.
-          //    // for normal use cases only node_modules is needed.
-          //    exclude: /node_modules|dev\/avalon|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
-          //    loader: 'babel'
-          //}
+    module : {
+        loaders   : [
+            cssLoader,
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+            //{
+            //    test: /\.js$/,
+            //    // excluding some local linked packages.
+            //    // for normal use cases only node_modules is needed.
+            //    exclude: /node_modules|dev\/avalon|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+            //    loader: 'babel'
+            //}
         ],
         preLoaders: [
             {test: /\.js$/, loader: "amdcss-loader"}
@@ -29,27 +29,31 @@ module.exports = {
     //    plugins: ['transform-runtime']
     //},
     resolve: {
-        extensions: ['.js',"",".css"],
-        alias: {
-            avalon: './avalon/avalon.shim',//在正常情况下我们以CommonJS风格引用avalon,以require('avalon')
+        extensions: ['.js', "", ".css"],
+        alias     : {
+            avalon     : './avalon/avalon.shim',//在正常情况下我们以CommonJS风格引用avalon,以require('avalon')
             "../avalon": './avalon/avalon.shim'//由于oniui都以是../avalon来引用avalon的，需要在这里进行别名
         }
     }
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
     module.exports.plugins = [
-      new webpack.DefinePlugin({
-          'process.env': {
-              NODE_ENV: '"production"'
-          }
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-          compress: {
-              warnings: false
-          }
-      }),
-      new webpack.optimize.OccurenceOrderPlugin()
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "common2",
+            minChunks: 2
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
     ]
 } else {
     module.exports.devtool = '#source-map'
