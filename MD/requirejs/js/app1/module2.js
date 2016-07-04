f06a5d5efc3001ecc4613b0e5851b15d0753c54e
LiuJun <870395192@qq.com>
2016-07-04 18:36:44 +0800

    require js

---
+++ MD/requirejs/js/app1/module2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Liu.Jun on 2016/7/4.
+ */
+define(function () {
+
+    console.log('Module2 ready !');
+    return {
+        name: 'module2'
+    }
+});
---
+++ MD/requirejs/js/app1/module3.js
@@ -0,0 +1,5 @@
+
+/**
+ * Created by Liu.Jun on 2016/7/4.
+ */
+'use strict'
