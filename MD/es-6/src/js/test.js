4e8e4ef35621b5b6bf1ad4b7642a03802a81eb96
Liu.Jun <870395192@qq.com>
2016-09-04 22:54:54 +0800

    webpack babel

---
+++ MD/es-6/src/js/test.js
@@ -0,0 +1,12 @@
+{
+    let a = 10;
+    var b = 1;
+
+    let data = [1, 2, 3, 4];
+
+    var testResult = data.map(function (item) {
+        return item * item;
+    });
+}
+
+
---
+++ MD/es-6/src/js/test2/test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by LiuJun on 2016/8/30.
+ */
+
+var x = 'test223';
