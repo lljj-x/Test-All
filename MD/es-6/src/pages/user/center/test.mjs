207750637c24a399ac1ec21343ad8593c98b0511
LiuJun <870395192@qq.com>
2016-09-09 18:00:38 +0800

    更新 webpack 配置

---
+++ MD/es-6/src/pages/user/center/test.mjs
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2016/9/9.
+ */
+console.log('我是xxxxxmjs');
--- MD/es-6/webpack.config.js
+++ MD/es-6/webpack.config.js
@@ -1,75 +1,84 @@
 var webpack = require('webpack');
-var cssLoader = {test: /\.css$/, loader: "style-loader!css-loader"};
 var path = require('path');
 var glob = require('glob');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-/**
- * 获得路径
- * @param globPath: str
- * @param pathDir: str 对比路径
- * @returns {{}}
- */
 function getEntry(globPath, pathDir) {
     var files = glob.sync(globPath);
     var entries = {},
         templates = {},
-        entry, dirname, basename, pathname, extname;
+        entry, dirname, basename, pathname, extname,outFileName;
 
     for (var i = 0; i < files.length; i++) {
         entry = files[i];
+
         dirname = path.dirname(entry);
         extname = path.extname(entry);
         basename = path.basename(entry, extname);
         pathname = path.normalize(path.join(dirname,  basename));
         pathDir = path.normalize(pathDir);
+        outFileName = path.normalize(dirname);
         if(pathname.startsWith(pathDir)){
             pathname = pathname.substring(pathDir.length)
         }
-        entries[pathname] = ['./' + entry];
+
+        if(outFileName.startsWith(pathDir)){
+            outFileName = outFileName.substring(pathDir.length)
+        }else{
+            outFileName = pathname;
+        }
+
+        // path window和liunx斜线相反 ...
+        pathname = pathname.replace(/\\/g,'/');
+        outFileName = outFileName.replace(/\\/g,'/');
+
+        entries['wpjs/' + outFileName] = ['./' + entry];
         templates[pathname] = {
-            chunks: ['vue-family', 'common-lib', basename],
-            filename: '' + pathname + '.html',
-            template: './src/pages/' + basename + '/' + basename + '.html',
+            chunks: ['wpjs/' + outFileName],
+            filename: '' + outFileName + '.html',
+            template: './src/pages/' + pathname + '.ejs',
             inject: 'body',
-            hash: true,
-            chunksSortMode: 'dependency'
+            chunksSortMode: 'dependency',
+            hash: false
         }
     }
-
     return {
         entries:entries,
         templates:templates
     };
 }
 
-
 var entries = getEntry('src/pages/**/*.js', 'src/pages/');
-console.log(entries);
+var htmlWebpackPluginConfig = (function (templates) {
+    var config = [];
+    for(var key in templates){
+        if(templates.hasOwnProperty(key)){
+            config.push(new HtmlWebpackPlugin(templates[key]));
+        }
+    }
+    return config;
+})(entries.templates);
 
-var htmlWebpackPluginConfig = '';
-
-
-
-
-module.exports = {
-    //entry  : './dev/index.js',
+var webPackConfig = {
     entry  : entries.entries,
-
     output : {
-        path      : path.join(__dirname, './dist'),
-        publicPath: 'dist/',
-        filename: '[name].build.js'
+        path      : path.join(__dirname, './build/'),
+        publicPath: '/',
+        // filename: '[name].[hash].js',
+        filename: '[name].js',
     },
     module : {
         loaders   : [
-            cssLoader,
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader?limit=8192'
             },
             {
-                test: /\.js$/,
+                test: /\.mjs$/,
                 // excluding some local linked packages.
                 // for normal use cases only node_modules is needed.
                 exclude: /node_modules|dev\/avalon|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
@@ -86,7 +95,7 @@ module.exports = {
             }
         ],
         preLoaders: [
-            //{test: /\.js$/, loader: "amdcss-loader"}
+
         ]
     },
     //devtool : '#eval',
@@ -96,28 +105,31 @@ module.exports = {
             avalon     : './avalon/avalon.shim',//在正常情况下我们以CommonJS风格引用avalon,以require('avalon')
             "../avalon": './avalon/avalon.shim'//由于oniui都以是../avalon来引用avalon的，需要在这里进行别名
         }
-    }
+    },
+    plugins: [].concat(htmlWebpackPluginConfig)
 };
 
-if (process.env.NODE_ENV === 'production') {
-
-    module.exports.plugins = [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "common2",
-            minChunks: 2
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-    ]
-} else {
-    module.exports.devtool = '#source-map'
-}
+module.exports = webPackConfig;
+//
+// if (process.env.NODE_ENV === 'production') {
+//
+//     module.exports.plugins = [
+//         new webpack.optimize.CommonsChunkPlugin({
+//             name: "common2",
+//             minChunks: 2
+//         }),
+//         new webpack.DefinePlugin({
+//             'process.env': {
+//                 NODE_ENV: '"production"'
+//             }
+//         }),
+//         new webpack.optimize.UglifyJsPlugin({
+//             compress: {
+//                 warnings: false
+//             }
+//         }),
+//         new webpack.optimize.OccurenceOrderPlugin()
+//     ]
+// } else {
+//     module.exports.devtool = '#source-map'
+// }
