1a47e541c0508d4c256203bf995845ae13865b0e
LiuJun <870395192@qq.com>
2016-09-09 18:00:38 +0800

    更新 webpack 配置

--- MD/es-6/src/js/test2/test.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by LiuJun on 2016/8/30.
- */
-
-var x = 'test223';
---
+++ MD/es-6/src/pages/user/center/center.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/9/9.
+ */
+require('./test.css');
+require('./test.mjs');
+console.log('Center just testxxxx');
