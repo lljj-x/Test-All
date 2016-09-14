8031174b19502be676e06622344babd15bcafaf3
liujunchina <asdf123456>
2016-09-14 11:12:37 +0800

    webpack 配置

--- MD/webpack-cli-liujun/src/pages/test1/test1.js
+++ MD/webpack-cli-liujun/src/pages/test1/test1.js
@@ -1,8 +1,7 @@
 /**
  * Created by LiuJun on 2016/8/30.
  */
-// require('scss/common.scss')
-require('./xx.mjs');
-require('./xx.scss');
+import './xx.mjs';
+import './xx.scss'
 
 var x = 'tesmt223';
