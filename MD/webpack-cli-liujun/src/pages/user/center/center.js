bad039e7ded6b95b8f28776b07c1da8f23989d66
liujunchina <asdf123456>
2016-09-14 16:42:39 +0800

    webpack 配置

--- MD/webpack-cli-liujun/src/pages/user/center/center.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2016/9/9.
- */
-import './test.css';
-import './test.mjs';
-
-console.log('Center just testxxxx');
--- MD/webpack-cli-liujun/src/pages/user/center/test.mjs
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Administrator on 2016/9/9.
- */
-console.log('我是xxxxxmjs');
