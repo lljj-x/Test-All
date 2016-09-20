9ae5d2dc6de1ffba8ff2f8ee2a3860065d744ffd
liujunchina <asdf123456>
2016-09-20 15:27:55 +0800

    webpack 配置

--- MD/webpack_dev_server/src/main.js
+++ MD/webpack_dev_server/src/main.js
@@ -1,7 +1,15 @@
 /**
  * Created by Liu.Jun on 2016/9/19.
  */
-alert('ddddddddddddddddd');
-alert('ddddddddddddddddd');
 
-
+import Vue from 'vue'
+import hellow from './Hello.vue'
+alert("fasdf");
+console.log("fadfsaf");
+new Vue({
+    el:'body',
+    test:'fasdf',
+    components:{
+        hellow:hellow
+    }
+});
