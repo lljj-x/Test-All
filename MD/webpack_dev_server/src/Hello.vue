7aa1eb84ad1d4bf70e78af9d8b41f850680e1f57
liujunchina <asdf123456>
2016-09-20 15:27:55 +0800

    webpack 配置

---
+++ MD/webpack_dev_server/src/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="example">{{ msg }}</div>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    data :function() {
+      return {
+        msg: 'Hello xxxxxxx!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .example {
+    color: red;
+  }
+</style>
